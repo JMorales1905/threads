d02f5a23a3e559f7d8d02e26cd2eebf71086a0de
JMorales1905 <Xweyin.21@gmail.com>
2024-07-18 15:07:59 -0700

    first test

---
+++ app/(root)/page.tsx
@@ -0,0 +1,33 @@
+import {
+    ClerkProvider,
+    SignedIn,
+    SignedOut,
+    SignInButton,
+    UserButton,
+} from "@clerk/nextjs";
+
+function Header() {
+    return (
+        <header style={{ display: "flex", justifyContent: "space-between", padding: 20 }}>
+            <h1>Threads</h1>
+            <SignedIn>
+                {/* Mount the UserButton component */}
+                <UserButton />
+            </SignedIn>
+            <SignedOut>
+                {/* Signed out users get sign in button */}
+                <SignInButton />
+            </SignedOut>
+        </header>
+    );
+}
+
+function MyApp() {
+    return (
+        <ClerkProvider>
+            <Header />
+        </ClerkProvider>
+    );
+}
+
+export default MyApp;
---
+++ middleware.ts
@@ -0,0 +1,13 @@
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
+
+const isProtectedRoute = createRouteMatcher([
+    '/', 'api/webhook/clerk'
+])
+
+export default clerkMiddleware()
+
+export const config = {
+    // The following matcher runs middleware on all routes
+    // except static assets.
+    matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
+};
