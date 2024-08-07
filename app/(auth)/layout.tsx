b393a5b7a80671696f6979c1df6d3fa1ac95e805
JMorales1905 <Xweyin.21@gmail.com>
2024-08-07 11:38:58 -0700

    testing homepage

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -5,8 +5,10 @@ import '../globals.css';
 
 export const metadata = {
     title: 'Threads',
-    description: 'A Next.js 13 Meta Threads Application'
+    description: 'A Next.js 13 Meta Threads Application',
 }
+
+
 
 const inter = Inter({ subsets: ["latin"] })
 
--- components/shared/RightSidebar.tsx
+++ components/shared/RightSidebar.tsx
@@ -1,6 +1,13 @@
 function RightSidebar() {
     return (
-        <h1>RightSidebar</h1>
+        <section className="custom-scrollbar rightsidebar">
+            <div className="flex flex-1 flex-col justify-start">
+                <h3 className="text-heading4-medium text-light-1">Suggestes Communities</h3>
+            </div>
+            <div className="flex flex-1 flex-col justify-start">
+                <h3 className="text-heading4-medium text-light-1">Suggestes Users</h3>
+            </div>
+        </section>
     )
 }
 
