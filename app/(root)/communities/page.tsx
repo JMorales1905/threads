39624e0110eb2b284abe049b2133baabf91b1d22
JMorales1905 <Xweyin.21@gmail.com>
2024-09-05 22:38:10 -0700

    final toturial commit

--- app/(root)/communities/page.tsx
+++ app/(root)/communities/page.tsx
@@ -1,7 +1,57 @@
-const Page = () => {
+import CommunityCard from '@/components/cards/CommunityCard';
+import UserCard from '@/components/cards/UserCard';
+import ProfileHeader from '@/components/shared/ProfileHeader';
+import { fetchCommunities } from '@/lib/actions/community.actions';
+import { fetchUser, fetchUsers } from '@/lib/actions/user.actions';
+import { currentUser } from '@clerk/nextjs/server';
+
+import Image from 'next/image';
+import { redirect } from 'next/navigation';
+
+async function Page() {
+    // checks if user exists
+    const user = await currentUser();
+    if (!user) return null;
+
+    // checks if user is onboarded
+    const userInfo = await fetchUser(user.id);
+    if (!userInfo?.onboarded) redirect('/onboarding');
+
+    // Fetch communities
+    const result = await fetchCommunities({
+        searchString: '',
+        pageNumber: 1,
+        pageSize: 25
+
+    });
+
     return (
-        <div className="head-text mb-10">Communities</div>
+        <section>
+            <h1 className="head-text mb-10">Search</h1>
+
+            {/* search bar */}
+
+            <div className='mt-14 flex flex-col gap-9'>
+                {result.communities.length === 0 ? (
+                    <p className='no-result'>No Users</p>
+                ) : (
+                    <>
+                        {result.communities.map((community) => (
+                            <CommunityCard
+                                key={community.id}
+                                id={community.id}
+                                name={community.name}
+                                username={community.username}
+                                imgUrl={community.image}
+                                bio={community.bio}
+                                members={community.members}
+                            />
+                        ))}
+                    </>
+                )}
+            </div>
+        </section>
     )
 }
 
-export default Page
+export default Page 
--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,10 +1,26 @@
-"use server"
+"use server";
 
-import { revalidatePath } from "next/cache";
-import User from "../models/user.model";
-import { connectToDB } from "../validations/mongoose"
-import Thread from "../models/thread.model";
 import { FilterQuery, SortOrder } from "mongoose";
+import { revalidatePath } from "next/cache";
+
+import Community from "../models/community.model";
+import Thread from "../models/thread.model";
+import User from "../models/user.model";
+
+import { connectToDB } from "../validations/mongoose";
+
+export async function fetchUser(userId: string) {
+    try {
+        connectToDB();
+
+        return await User.findOne({ id: userId }).populate({
+            path: "communities",
+            model: Community,
+        });
+    } catch (error: any) {
+        throw new Error(`Failed to fetch user: ${error.message}`);
+    }
+}
 
 interface Params {
     userId: string;
@@ -17,16 +33,15 @@ interface Params {
 
 export async function updateUser({
     userId,
-    username,
-    name,
     bio,
-    image,
+    name,
     path,
+    username,
+    image,
 }: Params): Promise<void> {
-    connectToDB();
-
-    // function updates the user
     try {
+        connectToDB();
+
         await User.findOneAndUpdate(
             { id: userId },
             {
@@ -36,100 +51,86 @@ export async function updateUser({
                 image,
                 onboarded: true,
             },
-            //operation that will update an existing row if specified value if a specified value already exists in table
-            //and will insert a new row if the specified value doesn't already exist.
-            {
-                upsert: true
-            }
+            { upsert: true }
         );
 
-        //NextJs function that allows to revalidate data associated with a specific task
-        if (path === '/profile/edit') {
+        if (path === "/profile/edit") {
             revalidatePath(path);
         }
-
     } catch (error: any) {
-        throw new Error(`Failed to create/update user: ${error.message}`)
+        throw new Error(`Failed to create/update user: ${error.message}`);
     }
 }
 
-export async function fetchUser(userId: string) {
+export async function fetchUserPosts(userId: string) {
     try {
         connectToDB();
 
-        return await User
-            .findOne({ id: userId })
-        // .populate({
-        // path: 'communities'
-        // model: Community
-        // })
-    } catch (error: any) {
-        throw new Error(`Failed to fetch user: ${error.message}`)
-    }
-}
-
-export async function fetchUserPost(userId: string) {
-    try {
-        connectToDB();
-
-        // Find all threads authored by user with the given userId
-
-        // TODO: Populate community
-        const threads = await User.findOne({ id: userId })
-            .populate({
-                path: 'threads',
-                model: Thread,
-                populate: {
-                    path: 'children',
+        // Find all threads authored by the user with the given userId
+        const threads = await User.findOne({ id: userId }).populate({
+            path: "threads",
+            model: Thread,
+            populate: [
+                {
+                    path: "community",
+                    model: Community,
+                    select: "name id image _id", // Select the "name" and "_id" fields from the "Community" model
+                },
+                {
+                    path: "children",
                     model: Thread,
                     populate: {
-                        path: 'author',
+                        path: "author",
                         model: User,
-                        select: 'name image id'
-                    }
-                }
-            })
-
+                        select: "name image id", // Select the "name" and "_id" fields from the "User" model
+                    },
+                },
+            ],
+        });
         return threads;
-    } catch (error: any) {
-        throw new Error(`Failed to fetch user posts: ${error.messsage}`)
+    } catch (error) {
+        console.error("Error fetching user threads:", error);
+        throw error;
     }
 }
 
+// Almost similar to Thead (search + pagination) and Community (search + pagination)
 export async function fetchUsers({
     userId,
     searchString = "",
     pageNumber = 1,
     pageSize = 20,
-    sortBy = "desc"
+    sortBy = "desc",
 }: {
     userId: string;
     searchString?: string;
     pageNumber?: number;
     pageSize?: number;
     sortBy?: SortOrder;
-}
-) {
+}) {
     try {
         connectToDB();
 
-        // calculates number of user to skip based on page number and size
+        // Calculate the number of users to skip based on the page number and page size.
         const skipAmount = (pageNumber - 1) * pageSize;
 
+        // Create a case-insensitive regular expression for the provided search string.
         const regex = new RegExp(searchString, "i");
 
-        // get user list
+        // Create an initial query object to filter users.
         const query: FilterQuery<typeof User> = {
-            id: { $ne: userId }
-        }
+            id: { $ne: userId }, // Exclude the current user from the results.
+        };
 
-        if (searchString.trim() !== '') {
+        // If the search string is not empty, add the $or operator to match either username or name fields.
+        if (searchString.trim() !== "") {
             query.$or = [
                 { username: { $regex: regex } },
-                { name: { regex: regex } }
-            ]
+                { name: { $regex: regex } },
+            ];
         }
 
+        // Define the sort options for the fetched users based on createdAt field and provided sort order.
         const sortOptions = { createdAt: sortBy };
 
         const usersQuery = User.find(query)
@@ -137,16 +138,18 @@ export async function fetchUsers({
             .skip(skipAmount)
             .limit(pageSize);
 
+        // Count the total number of users that match the search criteria (without pagination).
         const totalUsersCount = await User.countDocuments(query);
 
         const users = await usersQuery.exec();
 
+        // Check if there are more users beyond the current page.
         const isNext = totalUsersCount > skipAmount + users.length;
 
         return { users, isNext };
-
-    } catch (error: any) {
-        throw new Error(`Failed to fetch users: ${error.messsage}`)
+    } catch (error) {
+        console.error("Error fetching users:", error);
+        throw error;
     }
 }
 
@@ -154,29 +157,27 @@ export async function getActivity(userId: string) {
     try {
         connectToDB();
 
-        // find all threads created by the user
+        // Find all threads created by the user
         const userThreads = await Thread.find({ author: userId });
 
-
-        // Collect all the child thread ids (replies) from the 'children' 
-        // field and combines them into a single stored array
+        // Collect all the child thread ids (replies) from the 'children' field of each user thread
         const childThreadIds = userThreads.reduce((acc, userThread) => {
-            return acc.concat(userThread.children)
-        }, [])
+            return acc.concat(userThread.children);
+        }, []);
 
+        // Find and return the child threads (replies) excluding the ones created by the same user
         const replies = await Thread.find({
             _id: { $in: childThreadIds },
-            author: { $ne: userId }
+            author: { $ne: userId }, // Exclude threads authored by the same user
         }).populate({
-            path: 'author',
+            path: "author",
             model: User,
-            select: 'name image _id'
-        })
+            select: "name image _id",
+        });
 
         return replies;
-
-    } catch (error: any) {
-        throw new Error(`Failed to fetch activity: ${error.message}`);
-
+    } catch (error) {
+        console.error("Error fetching replies: ", error);
+        throw error;
     }
 }
